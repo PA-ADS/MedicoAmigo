6e41b86d24cca5ddfa3f780c7d30f6da9a044acb
Diogo Santos <diogo.vieira.santos@outlook.com.br>
2015-05-16 20:12:38 -0300

    Validações cadastro do Médico

--- app/models/medico.rb
+++ app/models/medico.rb
@@ -1,10 +1,7 @@
 class Medico < ActiveRecord::Base
-	## Regra de composição do email
-	EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 	
 	## Obriga preenchimento dos campos
 	validates_presence_of :nome, :cpf, :crm, :crm_uf, :especialidade_id
-	#validates_confirmation_of :email
 	
 	## Relacionamento das tabelas
 	belongs_to 	:especialidade
@@ -13,14 +10,5 @@ class Medico < ActiveRecord::Base
 	has_many 	:creditos
 
 	accepts_nested_attributes_for :usuario
-
-	## valida formato do email
-	#validate :email_format
-	
-	#private
-	## Trata erro de digitação do email
-	#def email_format
-	#	errors.add(:email, :invalid) unless email.match(EMAIL_REGEXP)
-	#end
 
 end
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,3 +1,22 @@
 class Usuario < ActiveRecord::Base
+	
+	# Regra de composição do email
+	EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+
+	# Validações Modelo
+	validates_presence_of :email, :password
+	validates_confirmation_of :email
+	validates :email, :uniqueness => true
+	# Valida formato do email
+	validate :email_format
+
+	# Relacionamento Modelos
 	has_one :medico
+	
+	private
+	# Trata erro de digitação do email
+	def email_format
+		errors.add(:email, :invalid) unless email.match(EMAIL_REGEXP)
+	end
+
 end
