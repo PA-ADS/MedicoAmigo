f34526b2af1bdbc8cb4b43f6952c41083df22fc6
luizfgmc <luizflavio@outlook.com.br>
2015-06-04 18:16:41 -0300

    Instalação gem RailsBrasilian + validação de cpf paciente

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'bcrypt', '~> 3.1.7'
 # restricts what resources a given user is allowed to access
 gem 'cancancan', '~> 1.10'
 
+gem 'brazilian-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
@@ -45,5 +47,6 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
 end
 
--- app/models/medico.rb
+++ app/models/medico.rb
@@ -1,9 +1,12 @@
 class Medico < ActiveRecord::Base
+	after_validation :tratarcpf , on: :create
+
 	
 	## Validação de campos do formulario
 	validates_presence_of :nome, :cpf, :crm, :crm_uf, :especialidade_id
 	validates :nome, :length => {:in => 2..60}
-	validates :cpf, :length => {:is => 11}
+	usar_como_cpf :cpf
+	validates_uniqueness_of :cpf
 	validates :crm, :length => {:maximum => 10}
 
 	
@@ -16,5 +19,12 @@ class Medico < ActiveRecord::Base
 	has_many	:agendas, :dependent => :destroy
 
 	accepts_nested_attributes_for :usuario
+
+	def tratarcpf
+		write_attribute(:cpf , self.cpf.to_s.delete!('.-'))
+		
+		puts "teste"
+		puts cpf
+	end
 
 end
