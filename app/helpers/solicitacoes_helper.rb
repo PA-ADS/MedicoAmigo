8815a923b004c4ed923eddf515e461bf9ea0a2e3
Diogo Santos <diogo.vieira.santos@outlook.com.br>
2015-06-30 20:54:54 -0300

    Alteração views solicitação bootstrap

--- app/helpers/solicitacoes_helper.rb
+++ app/helpers/solicitacoes_helper.rb
@@ -4,4 +4,21 @@ module SolicitacoesHelper
 			time.to_s(:time)
 		end
 	end
+
+	def situacao status
+		if  status == "PE"
+			"Pendente"
+		elsif status == "AP"
+			"Aprovada"
+		elsif status == "RJ"
+			"Rejeitada"
+		elsif status == "FS"
+			"Finalizada sucesso"
+		elsif status == "FN"
+			"Finalizada s/ sucesso"
+		else
+			""
+		end
+	end
+	
 end
