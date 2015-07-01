0513a780a15e679507e3c0eb6dd3059915a64b70
Diogo Santos <diogo.vieira.santos@outlook.com.br>
2015-07-01 20:23:05 -0300

    Tratado os erros acesso geral

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,13 @@ class ApplicationController < ActionController::Base
 
 	helper_method :usuario_corrente
 
+	rescue_from CanCan::AccessDenied do |exception|
+	    respond_to do |format|
+	      format.json { render nothing: true, status: :forbidden }
+	      format.html { redirect_to root_path, :alert => exception.message }
+	    end
+  	end
+
 	private
 
 		def current_ability
